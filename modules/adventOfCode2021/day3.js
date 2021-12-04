70ceda5dec8d0e2f066e79a58d7c2a07fbfea5ca
philipp <philipp.rembold@gmail.com>
2021-12-04 15:44:13 +0100

    not working

--- modules/adventOfCode2021/day3.js
+++ modules/adventOfCode2021/day3.js
@@ -31,29 +31,25 @@ function initMetaData(input) {
 
 function solve2(input) {
     let report = []
-    let determineCharToCheck = (meta, i) => {
-        return meta.count[i] >= meta.half ? "1" : "0";
-    }
     report = input.map(x => x)
+    let oxGenRating = filterWithFunction(report, (meta, i) => {
+        return meta.count[i] >= meta.half ? "1" : "0";
+    })
+    let co2ScrubberRating = filterWithFunction(report, (meta, i) => {
+        return meta.count[i] <= meta.half ? "1" : "0";
+    })
 
+    return co2ScrubberRating * oxGenRating
+}
+
+function filterWithFunction(report, determineCharToCheck) {
     for (let i = 0; i < report[0].length; i++) {
         let meta = initMetaData(report);
         countBits(report, meta.count);
         report = report.filter(s => s.charAt(i) == determineCharToCheck(meta, i))
         if (report.length == 1) { break }
     }
-    let oxGenRating = parseInt(report[0], 2)
-
-    report = input.map(x => x)
-    for (let i = 0; i < report[0].length; i++) {
-        let meta = initMetaData(report);
-        countBits(report, meta.count);
-        let c = meta.count[i] <= meta.half ? "1" : "0"
-        report = report.filter(s => s.charAt(i) == c)
-        if (report.length == 1) { break }
-    }
-    let co2ScrubberRating = parseInt(report[0], 2)
-    return co2ScrubberRating * oxGenRating
+    return parseInt(report[0], 2)
 }
 
 module.exports = { solve1, solve2 }
